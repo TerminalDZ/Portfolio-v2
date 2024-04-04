0d4150206cd684ad3dd831d1ec075ff561f5ced3
Idriss Boukmouche <69165137+TerminalDZ@users.noreply.github.com>
2024-04-04 08:52:27 +0200

    Update Form.tsx

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -73,7 +73,7 @@ export function Form() {
           errors={state.errors}
         />
         <ReCAPTCHA
-          sitekey="6Lfj9NYfAAAAAP8wPLtzrsSZeACIcGgwuEIRvbSg"
+          sitekey="6Ldap64pAAAAADRBFQUZ5DViD-mXSM-DFqwbARQ-"
           onChange={(e) => {
             setIsHuman(true)
           }}
