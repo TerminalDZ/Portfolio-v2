fa7933a39cc20c5990fb190a6f876e5bcd5b4d91
Idriss Boukmouche <69165137+TerminalDZ@users.noreply.github.com>
2024-04-04 08:10:38 +0200

    Update Contact.tsx

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -15,12 +15,12 @@ export function Contact(){
       </header>
       <div className="contacts">
         <div>
-        <a href="mailto:Hello@vinayaksingh.com"><img src={emailIcon} alt="Email" /></a> 
-          <a href="mailto:Hello@vinayaksingh.com">Hello@vinayaksingh.com</a>
+        <a href="mailto:boukemoucheidriss@gmail.com"><img src={emailIcon} alt="Email" /></a> 
+          <a href="mailto:boukemoucheidriss@gmail.com">boukemoucheidriss@gmail.com</a>
         </div>
         <div>
-        <a href="tel:+919630576848"><img src={phoneIcon} alt="Phone No" /></a>
-          <a href="tel:+919630576848">(+91) 9630576848</a>
+        <a href="tel:+213558601124"><img src={phoneIcon} alt="Phone No" /></a>
+          <a href="tel:+213558601124">(+213) 558601124</a>
         </div>  
       </div>
       <Form></Form>
