19308fd3368f61146d4616a424328d1c9da88719
Idriss Boukmouche <69165137+TerminalDZ@users.noreply.github.com>
2024-04-04 08:50:24 +0200

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,15 +5,13 @@ import githubIcon from '../../assets/github.svg'
 import whatsapp from '../../assets/whatsapp.svg'
 import telegram from '../../assets/telegram.svg'
 import instagramIcon from '../../assets/instagram.svg'
+import facebookIcon from '../../assets/facebook-3-2.svg'
 // import discordIcon from '../../assets/discord.png'
 
 export function Footer() {
   return (
     <Container className="footer">
-      <a href="https://vinayaksingh.in" className="logo">
-        <span>www.vinayak</span>
-        <span>singh.in</span>
-      </a>
+   
       <div>
         <p>
           This Website was made with <img src={reactIcon} alt="React" />
@@ -22,35 +20,29 @@ export function Footer() {
       </div>
       <div className="social-media">
         <a
-          href="https://www.linkedin.com/in/codevinayak"
+          href="https://www.facebook.com/idriss.boukmouche/"
           target="_blank"
           rel="noreferrer"
         >
-          <img src={linkedin} alt="Linkedin" />
+          <img src={facebookIcon} alt="Facebook" />
         </a>
         <a
-          href="https://github.com/CodeVinayak/"
+          href="https://github.com/terminalDZ/"
           target="_blank"
           rel="noreferrer"
         >
           <img src={githubIcon} alt="GitHub" />
         </a>
         <a
-          href="https://api.whatsapp.com/send/?phone=%2B919630576848&text=Hello+Vinayak"
+          href="https://api.whatsapp.com/send/?phone=%2B213558601124&text=Hello+Idriss+Boukmouche"
           target="_blank"
           rel="noreferrer"
         >
           <img src={whatsapp} alt="Whatsapp" />
         </a>
+        
         <a
-          href="https://t.me/CodeVinayak"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={telegram} alt="telegram" />
-        </a>
-        <a
-          href="https://www.instagram.com/vinayaksingh.in"
+          href="https://www.instagram.com/idriss_boukmouche/"
           target="_blank"
           rel="noreferrer"
         >
